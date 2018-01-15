8ad37afd3e6e9edb7cf2f8f8bfd97c6616911842
tangtantivirun <tang2tasketball@gmail.com>
2018-01-15 15:49:21 -0500

    Login Created

--- PeerTutor/LoggedInViewController.swift
+++ PeerTutor/LoggedInViewController.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class LoggedInViewController: UIViewController {
+    @IBAction func logoutTapped(sender: AnyObject) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoggedInViewController")
+                present(vc, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
